3ea97d7da622b95570926aa88efc7f0cbd1539a3
Mauriciofcj123 <72559455+Mauriciofcj123@users.noreply.github.com>
2024-08-01 10:03:23 -0400

    1

--- index23.php
+++
@@ -1,3 +0,0 @@
-<?php
-    header('location: inicio/index.php');
-?>
---
+++ scriptc.js
@@ -0,0 +1,17 @@
+﻿var MenuVisible=false;
+let Menu=document.getElementById('Menu');
+Menu.style.opacity='0%;';
+Menu.style.height='0px';
+function Hamburger(){
+    MenuVisible=!MenuVisible;
+
+    if(MenuVisible){
+        Menu.style.height='150px';
+        Menu.style.opacity='100%;';
+    }else{
+        
+        Menu.style.opacity='0%;';
+        Menu.style.height='0px';
+        
+    }
+}
